ab835981fb45dd04364421e27c6d9b0b97ba2e64
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-18 17:10:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,6 +22,6 @@ class School
   end 
   
   def sorted_list_of_names
-    @roster.each {|student_name, grade_level| student_name = sort}
+    @roster.each {|grade_level, student_name| student_name = sort}
   end 
 end 

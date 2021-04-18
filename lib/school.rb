cca2aac2acc54988cf402149c6504f3a9e4dff18
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-18 16:53:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,16 @@
 # code here!
 
 class School
+  attr_reader :school_name, :roster
+  
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end 
   
   def add_student(student_name, grade)
-    roster["#{student_name}"] = []
-    roster["#{student_name}"] << "#{grade}"
+    @student_name = student_name
+    @grade = grade
+    if @roster.
   end 
 end 
